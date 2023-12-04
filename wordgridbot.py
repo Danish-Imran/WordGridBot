0b370840e5e0a1c5ac3690a0aaa4397a47ecbc18
Danish-Imran <100322051+Danish-Imran@users.noreply.github.com>
2023-12-03 19:02:25 -0500

    Update wordgridbot.py

--- wordgridbot.py
+++ wordgridbot.py
@@ -1,7 +1,6 @@
-import math
-from dictionary import word_list
+from dictionary import initialize_dictionary
 
-point_values = {
+point_values = { # original values for each letter
     "A": 1,
     "B": 3,
     "C": 3,
@@ -30,16 +29,37 @@ point_values = {
     "Z": 10,
 }
 
-def calculate_point_value(input_word):
-    scoring_word = input_word
-    word_point_total = 0
+def calculate_point_value(input_word): 
+    # calculates the points gained from entering a word
+    scoring_word = input_word # creates duplicate
+    word_point_total = 0 # initializes score tracker and resets it
 
-    for letter in scoring_word:
-        for i in range(1, scoring_word.count(letter) + 1):
-            word_point_total += point_values[letter] * i
-            scoring_word = scoring_word.replace(letter, "")
-            
-    return input_word
+    for letter in scoring_word: 
+        for i in range(1, scoring_word.count(letter) + 1): # iterates through amount of repeated letters
+            word_point_total += point_values[letter] * i # adds the score for that letter
+            scoring_word = scoring_word.replace(letter, "") # removes letter from string so it is not counted twice
 
-    
-calculate_point_value("COOCOO")
+
+    return word_point_total
+
+def is_word_formable(word, letters):
+    # sees if a word is formable with the given letters
+    word_set = set(word)
+    letters_set = set(letters)
+
+    return word_set.issubset(letters_set) # returns boolean
+
+
+word_list = initialize_dictionary() # creates dictionary
+
+
+# begin game loop
+
+nine_letters = "PAZRMTEUK"
+possible_words = []
+
+for word in word_list:
+    if is_word_formable(word, nine_letters):
+        possible_words.append(word)
+
+possible_words = sorted(possible_words, key=calculate_point_value) # sorts lowest to highest point value
