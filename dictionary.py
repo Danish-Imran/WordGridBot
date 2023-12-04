69c9ba1cbc05eef668ae36653efc04ce5892aa79
Danish-Imran <100322051+Danish-Imran@users.noreply.github.com>
2023-12-03 19:02:38 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,9 @@
 from nltk.corpus import words
-word_list = words.words()
 
-word_list.extend(("pazazz", "razzamatazz", "coocoo", "booboo", "zyzzyva", "muumuu", "deeded"))
+def initialize_dictionary():
+    global word_list
+    english_words = words.words()
+    english_words.extend(("pazazz", "razzamatazz", "coocoo", "booboo", "zyzzyva", "muumuu", "deeded")) # add beneficial words that aren't included in dictionary
+
+    word_list = [word.upper() for word in english_words if len(word) > 3]
+    return word_list
